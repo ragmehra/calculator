61ca959a0576562b9b77d741789675906bc2bcbd
Raghav Mehra <ragmehra@hotmail.com>
2021-12-25 15:50:27 -0800

    Create operate()

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
+function operate (operator, num1, num2) {
+    if (operator === "+") return add(num1, num2);
+    else if (operator === "-") return subtract(num1, num2);
+    else if (operator === "*") return multiply(num1, num2);
+    else if (operator === "/") return divide(num1, num2);
+}
+
