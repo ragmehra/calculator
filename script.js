b40e54fbea01836c1a6858785f28a64226ecd38a
Raghav Mehra <ragmehra@hotmail.com>
2021-12-26 10:51:49 -0800

    Enable the calculator to actually compute values

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let displayContent = "";
+let contentVariable = "";
+let num1 = null; 
+let num2 = null;
+let operator = null;
 
 function add (num1, num2) {
-    return num1 * num2;
+    return +num1 + +num2;
 }
 
 function subtract (num1, num2) {
@@ -23,22 +27,58 @@ function operate (operator, num1, num2) {
     else if (operator === "/") return divide(num1, num2);
 }
 
-function parseDisplay () {
-    
+function reset () {
+        displayContent = "";
+        contentVariable = "";
+        operator = null;
+        num1 = null;
+        num2 = null;
+}
+
+function compute () {
+    num2 = contentVariable;
+
+    console.log("Num1: ", num1);
+    console.log("Operator: ", operator);
+    console.log("Num2: ", num2);
+
+    displayContent = operate(operator, num1, num2);
+    let display = document.querySelector("#display");
+    display.textContent = displayContent;
+    contentVariable = "";
+    operator = null;
+
+    console.log("Num1: ", num1);
+    console.log("Operator: ", operator);
+    console.log("Num2: ", num2);
+
 }
 
 function updateDisplay (e) {
     let display = document.querySelector("#display");
     if (e.target.value === "clear") {
-        displayContent = ""
+        reset();
     }
     else if (e.target.value === "delete") {
         displayContent = displayContent.slice(0, displayContent.length - 1);
+        contentVariable = contentVariable.slice(0, displayContent.length - 1);
     }
-    else {
+    else if (e && e.target.value !== "=" && 
+                !e.target.classList.contains("operator")) {
         displayContent += e.target.value;
-        console.log(e);
+        contentVariable += e.target.value;
     }
+    else if (e && e.target.value !== "=" && 
+    e.target.classList.contains("operator") && !operator) {
+        num1 = displayContent;
+        operator = e.target.value;
+        displayContent += e.target.value;
+        contentVariable = "";
+    }
+    
+    
+    console.log("Display Content: ", displayContent);
+    console.log("Content Variable: ", contentVariable);
     display.textContent = displayContent;
 }
 
@@ -46,4 +86,7 @@ let columns = document.querySelectorAll(".column");
 columns.forEach( (column) => {
     column.addEventListener("click", updateDisplay);
 })
-console.log(columns);
+
+let equal = document.querySelector("#equal");
+equal.addEventListener("click", compute);
+
