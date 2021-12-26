23b19a748b24b744e4ac5dadd2c3e1ed6e3cb0f9
Raghav Mehra <ragmehra@hotmail.com>
2021-12-26 12:45:30 -0800

    Add floating point capability

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ let contentVariable = "";
 let num1 = null; 
 let num2 = null;
 let operator = null;
-let index;
+let index = null;
+let decimalIndicator = false;
+let decimalIndex = null;
 
+
+//Basic Math Functions:
 function add (num1, num2) {
     return +num1 + +num2;
 }
@@ -28,14 +32,18 @@ function operate (operator, num1, num2) {
     else if (operator === "/") return divide(num1, num2);
 }
 
+//Resets all global variables:
 function reset () {
         displayContent = "";
         contentVariable = "";
         operator = null;
         num1 = null;
         num2 = null;
+        decimalIndicator = false;
+        index = null;
 }
 
+//Runs operate and sets up display for next computation:
 function compute () {
     let pass = false;
     num2 = contentVariable;
@@ -54,8 +62,6 @@ function compute () {
         pass = true;
     }
     
-    
-
     console.log("Num1: ", num1);
     console.log("Operator: ", operator);
     console.log("Num2: ", num2);
@@ -63,6 +69,7 @@ function compute () {
     return pass;
 }
 
+//Manages display:
 function updateDisplay (e) {
     let display = document.querySelector("#display p");
     if (e.target.value === "clear") {
@@ -70,22 +77,43 @@ function updateDisplay (e) {
     }
     else if (e.target.value === "delete") {
         console.log("DisplayContent.length - 1: ", displayContent.length-1);
+        //If we're deleting an operator, reset the operator variables
         if ((displayContent.length-1) === index)
         {
             operator = null;
         }
+
+        //If we're deleting a decimal, reset the decimal variables
+        if ((displayContent.length-1) === decimalIndex)
+        {
+            decimalIndex = null;
+            decimalIndicator = false;
+        }
+        
         console.log("Display Content: ", displayContent);
         displayContent = displayContent.slice(0, displayContent.length - 1);
         contentVariable = contentVariable.slice(0, contentVariable.length - 1);
     }
+    //Check for number click
     else if (e && e.target.value !== "=" && 
                 !e.target.classList.contains("operator")) {
-        displayContent += e.target.value;
-        contentVariable += e.target.value;
+        //setup first decimal use
+        if (e.target.value === "." && !decimalIndicator) {
+            decimalIndicator = true;
+            decimalIndex = displayContent.length;
+            displayContent += e.target.value;
+            contentVariable += e.target.value;
+        }
+        else if (e.target.value !== ".") {
+            displayContent += e.target.value;
+            contentVariable += e.target.value;
+        }
+    
     }
+    //Check for operator click
     else if (e && e.target.value !== "=" && 
     e.target.classList.contains("operator")) {
-        
+        //Run computation if running operator again
         if (operator) {
             let pass = compute();
             if (pass) {
@@ -110,6 +138,7 @@ function updateDisplay (e) {
     display.textContent = displayContent;
 }
 
+//Setup listeners:
 let columns = document.querySelectorAll(".column");
 columns.forEach( (column) => {
     column.addEventListener("click", updateDisplay);
