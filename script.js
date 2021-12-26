75e03dcc0c3ce0b345f96daaf6d39d03842a46f2
Raghav Mehra <ragmehra@hotmail.com>
2021-12-26 08:52:16 -0800

    Enable display functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayContent = "";
+
 function add (num1, num2) {
     return num1 * num2;
 }
@@ -21,3 +23,27 @@ function operate (operator, num1, num2) {
     else if (operator === "/") return divide(num1, num2);
 }
 
+function parseDisplay () {
+    
+}
+
+function updateDisplay (e) {
+    let display = document.querySelector("#display");
+    if (e.target.value === "clear") {
+        displayContent = ""
+    }
+    else if (e.target.value === "delete") {
+        displayContent = displayContent.slice(0, displayContent.length - 1);
+    }
+    else {
+        displayContent += e.target.value;
+        console.log(e);
+    }
+    display.textContent = displayContent;
+}
+
+let columns = document.querySelectorAll(".column");
+columns.forEach( (column) => {
+    column.addEventListener("click", updateDisplay);
+})
+console.log(columns);
